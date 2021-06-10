b097218168f2408fbd9a0106fdfc3167e145a5f2
Turan Ates <jamesdusky@dustytavern.com>
2021-06-10 18:12:53 -0400

    Debug

--- main.js
+++ main.js
@@ -228,7 +228,7 @@ function update(key){
             promiseSearch({status:0});
           }else{
             if(Object.size(user.data) > 0){
-              console.log({key:user.data.user_key, value:user.data.value}});
+              console.log({key:user.data.user_key, value:user.data.value});
               promiseSearch({ status:1, data:{ key:user.data.user_key, value:user.data.value } });
             }else{
               promiseSearch({ status:0 });
