767f41c051b9aa66a799ad015c1b90a7961ce152
Turan Ates <jamesdusky@dustytavern.com>
2021-06-26 13:45:13 -0400

    Debug

--- main.js
+++ main.js
@@ -501,6 +501,11 @@ function orgPlayers(sid, page){
         if(user){
           if(Object.size(user.data) > 0){
             console.log(user.data);
+            var result = [];
+            user.data.forEach((item, i) => {
+              result.push(item.handle);
+            });
+            console.log(result);
             //callback({ status:1, data:grossPages });
           }else{
             callback({ status:0, data:sid+" not found." });
