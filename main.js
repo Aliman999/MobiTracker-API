eb019c561d44dc159e06906833b4887e6ab27a84
Turan Ates <jamesdusky@dustytavern.com>
2021-06-26 13:39:42 -0400

    Debug

--- main.js
+++ main.js
@@ -175,7 +175,7 @@ wss.on('connection', function(ws){
       ws.user = "Scanner";
       ws.isAlive = true;
       ws.orgResponse = [];
-      console.log(wss.clients);
+      console.log(wss.clients.length);
       console.log(ws.user+" Connected");
       ws.on('job', function(data){
         var org, length;
