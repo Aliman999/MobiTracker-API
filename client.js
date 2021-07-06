39082ef55cbd7811140e36fb122a0c98663174b8
Turan Ates <jamesdusky@dustytavern.com>
2021-07-05 22:22:47 -0400

    Debug

--- client.js
+++ client.js
@@ -6,34 +6,43 @@ const fs = require('fs');
 var publicKey = fs.readFileSync('api.key.pub');
 
 function socket(){
-  var payload = jwt.sign({exp:Math.floor(Date.now() / 1000) + (60 * 60),foo:"bar"}, publicKey, { algorithm: 'RS256' });
+  var payload = jwt.sign({exp:Math.floor(Date.now() / 1000) + (60 * 60)}, publicKey, { algorithm: 'RS256' });
   var message;
-  webSocket = new WebSocket("wss://ws.mobitracker.co:2599");
-  webSocket.onopen = function(){
+  ws = new WebSocket("wss://ws.mobitracker.co:2599");
+  ws.onopen = function(){
     console.log("Connected to Internal API");
     message = {
       type:"rsa",
       token:{ org:"evilorg", jwt:payload }
     };
-    webSocket.send(JSON.stringify(message));
+    ws.send(JSON.stringify(message));
     heartbeat();
   }
-  webSocket.onerror = function(err){
+  ws.onerror = function(err){
   }
-  webSocket.onclose = function(){
+  ws.onclose = function(){
     console.log("Lost Connection to Internal API");
     setTimeout(socket, 3000);
   };
 
-  webSocket.onmessage = function(data){
+  ws.onmessage = function(data){
+    data = JSON.parse(data.data);
     console.log(data);
   }
 
   function heartbeat() {
-    if (!webSocket) return;
-    if (webSocket.readyState !== 1) return;
-    webSocket.send(JSON.stringify({type:"ping"}));
+    if (!ws) return;
+    if (ws.readyState !== 1) return;
+    ws.send(JSON.stringify({type:"ping"}));
     setTimeout(heartbeat, 3000);
+  }
+
+  function user(user){
+    message = {
+      type:"user",
+      data:"JamesDusky"
+    }
+    ws.send(JSON.stringify(message));
   }
 }
 
