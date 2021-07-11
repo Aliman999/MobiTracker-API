d3f86126acca1dbd21b736e9b2d9c0ec757b611b
Turan Ates <jamesdusky@dustytavern.com>
2021-07-11 16:38:40 -0400

    Debug

--- api.js
+++ api.js
@@ -102,7 +102,7 @@ if(server.listen(2599)){
 function toEvent(message){
   var event = JSON.parse(message);
   console.log(event);
-  this.emit(event);
+  this.emit(...event);
 }
 
 function heartbeat(){
