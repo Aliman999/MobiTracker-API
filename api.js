4a5f43bfcdaa495fc632cce43d78dea3ce1f7966
Turan Ates <jamesdusky@dustytavern.com>
2021-07-16 11:17:26 -0400

    Debug

--- api.js
+++ api.js
@@ -370,6 +370,12 @@ wss.on('connection', function(ws){
           message: "Authenticated",
           status: 1
         }));
+        ws.send(JSON.stringify({
+          type: "update",
+          data: adminPanel.get("panelStatus"),
+          message: "Success",
+          status: 1
+        }));
         setInterval(() => {
           if (scanner) {
             ws.send(JSON.stringify({
