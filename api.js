23f09aabc2e75c397fe08c6a02fe10036d4f2067
Turan Ates <jamesdusky@dustytavern.com>
2021-08-07 16:08:56 -0400

    Debug

--- api.js
+++ api.js
@@ -285,7 +285,7 @@ var api = {
                 const symbol = part.added ? ' + ' :
                   part.removed ? ' - ' : ' @@ '
                 
-                changesOutput += "<p class='"+color+"'>"+symbol+part.value.join(""+symbol)+"</p>";
+                changesOutput += "<p class='"+color+"'>"+symbol+part.value.join("\n"+symbol)+"</p>";
               });
               result[i] = { title: item.event, description: events, day: dayStamp, month: monthStamp, date: dateStamp, time: timeStamp, direction: direction, extra: changesOutput, actions: [{ text: "View Bio", href: "" }]};
             }
