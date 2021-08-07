a30d3ba884ff8384e7c2a436488183bdc4a0123c
Turan Ates <jamesdusky@dustytavern.com>
2021-08-07 12:00:01 -0400

    Debug

--- api.js
+++ api.js
@@ -262,7 +262,8 @@ var api = {
               events = item.username+" changed their avatar.";
               result[i] = { title: item.event, description: events, day: dayStamp, month: monthStamp, date: dateStamp, time: timeStamp, direction: direction, extra: { old: saved[i - 1].avatar, new: item.avatar }, actions: [{ text: "View Bio", href: "" }]};
             }else if(item.event === "Bio Changed"){
-              events = item.username+" changed their bio.";
+              events = item.username + " changed their bio.";
+              console.log({ old: saved[i - 1].bio, new: item.bio});
               const tempOldBio = JSON.parse(saved[i - 1].bio);
               const tempNewBio = JSON.parse(item.bio);
               var changes = Diff.diffChars(tempOldBio, tempNewBio);
