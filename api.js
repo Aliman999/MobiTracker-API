fa36b38598f2b7873e399bff08aafa816464260d
Turan Ates <jamesdusky@dustytavern.com>
2021-09-05 12:49:33 -0400

    Debug

--- api.js
+++ api.js
@@ -393,7 +393,7 @@ wss.on('connection', function(ws){
   ws.on('message', toEvent)
     .on('ping', heartbeat)
     .on('internal', function (data){
-      console.log(typeof wss.clients);
+      console.log(wss.clients.size);
       jwt.verify(data, config.Secret, { algorithm: 'HS265' }, function(err, decoded){
         if(err){
           console.log(err);
